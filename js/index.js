9523b83e6c223f9064af3edbf8a454b1588c92ad
sravanVJ <sravan.v1993@gmail.com>
2020-08-23 16:44:15 +0530

    Clean Code

--- js/index.js
+++ js/index.js
@@ -1,14 +1,100 @@
 let totalExpense = 0;
+const allExpenses = [];
 
-const btnElement = document.querySelector("#btnAddExpense");
-const inputElement = document.querySelector("#inputAmount");
-const headingElement = document.querySelector("#headingTotal");
+// Get DOM elements by reference
+const addExpense = document.querySelector("#button-add-expense"),
+  amountSpent = document.querySelector("#amount-spent"),
+  spentOn = document.querySelector("#spent-on"),
+  totalExpenses = document.querySelector("#total-expenses"),
+  expenseTable = document.querySelector("#expense-table");
 
-btnElement.addEventListener("click", addExpenseToTotal, false);
-headingElement.textContent = totalExpense;
+// Initialize TotalExpense on page load
+function init() {
+  _setTotalExpense(totalExpense);
+}
 
-function addExpenseToTotal() {
-  const amount = Number(inputElement.value);
-  totalExpense = totalExpense + amount;
-  headingElement.textContent = totalExpense;
+/** Controller Functions */
+
+// Calculate Total Expenses
+function _calculateExpenseTotal() {
+  if (amountSpent.value !== "" && spentOn.value !== "") {
+    const expenseItem = {},
+      amount = Number(amountSpent.value),
+      description = spentOn.value;
+
+    expenseItem.amount = amount;
+    expenseItem.description = description;
+    expenseItem.moment = new Date();
+
+    allExpenses.push(expenseItem);
+
+    // Render Expense List
+    _generateExpenseList(allExpenses);
+    // Clear form data after expense added to the list
+    _clearForm();
+
+    totalExpense = totalExpense + amount;
+    _setTotalExpense(totalExpense);
+  }
+}
+
+// Clear Form Data
+function _clearForm() {
+  (amountSpent.value = ""), (spentOn.value = "");
+}
+
+// Set Total Expenses
+function _setTotalExpense() {
+  let totalExpenseLabel = `Total: ${totalExpense}`;
+  totalExpenses.textContent = totalExpenseLabel;
+}
+
+// Get Date in the desired format [MMM DD, YYYY]
+function _getDateString(moment) {
+  const options = { year: "numeric", month: "long", day: "numeric" };
+  return moment.toLocaleDateString("en-US", options);
+}
+
+// Delete Expense
+function _deleteExpense(expenseItem) {
+  allExpenses.forEach((expense, index) => {
+    if (expense.moment.valueOf() === expenseItem) {
+      totalExpense = totalExpense - expense.amount;
+      allExpenses.splice(index, 1);
+    } else {
+      return allExpenses;
+    }
+  });
+  _generateExpenseList(allExpenses);
+  _setTotalExpense(totalExpense);
+}
+
+// Generate Expense List
+function _generateExpenseList(expenseList) {
+  const expenseListContent = expenseList
+    .map((expense) => _createListItem(expense))
+    .join("");
+  expenseTable.innerHTML = expenseListContent;
+}
+
+/** VIEW Layer */
+
+// Create Expense Item
+function _createListItem({ description, amount, moment }) {
+  return `
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+    <div class="d-flex flex-column">
+      ${description}
+      <small class="text-muted">${_getDateString(moment)}</small>
+    </div>
+    <div>
+      <span class="px-5">
+      ₹ ${amount}
+      </span>
+      <button type="button" class="btn btn-outline-danger btn-sm" onclick="_deleteExpense(${moment.valueOf()})">
+        <i class="fas fa-trash-alt"></i>
+      </button>
+    </div>
+  </li>
+  `;
 }
